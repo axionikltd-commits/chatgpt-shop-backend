0d70fcaa7b5c863ee0126d2c4a7c4a4d93957689
Chaitanya <axionikltd-commits>
2026-02-07 16:05:50 +0530

    Removed email notify utils

--- utils/templates.js
+++
@@ -1,8 +0,0 @@
-export function orderCreatedEmail(order) {
-  return `
-    <h2>🛍️ Order Confirmed</h2>
-    <p>Order ID: <b>${order.orderId}</b></p>
-    <p>Status: ${order.deliveryStatus}</p>
-    <p>We’ll notify you when it ships 🚚</p>
-  `;
-}
